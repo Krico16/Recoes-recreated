85368bc46ed2698d3700f2e74656d0f51cccd41d
Angel Castillo <bash_stone_u@hotmail.com>
2021-11-23 00:52:48 -0500

    Updated headers

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 
 var app = express();
 
+app.disable('X-Powered-By:')
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
